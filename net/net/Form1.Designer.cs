e4e98ec21801fe24fc5dbd8ff6076df8b4f91016
p4p4 <p4p4p4@web.de>
2011-02-17 15:15:49 +0100

    net

---
+++ net/net/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            pickle();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripSeparator1,
+            this.toolStripTextBox2,
+            this.toolStripButton1,
+            this.toolStripButton3,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(551, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(300, 25);
+            this.toolStripTextBox1.ToolTipText = "Server Name";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox2.ToolTipText = "Port No";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(51, 22);
+            this.toolStripButton1.Text = "Connect";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButton3.Text = "Clear";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripButton2.CheckOnClick = true;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 22);
+            this.toolStripButton2.Text = "Rec";
+            this.toolStripButton2.ToolTipText = "record output";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(0, 46);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(551, 292);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(551, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 337);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "net";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripComboBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripTextBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+
+    }
+}
+
---
+++ net/net/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace net
+{
+    public partial class Form1 : Form
+    {
+        static int nc = 0;
+        private System.Net.Sockets.TcpClient sock;
+        System.IO.StreamWriter logFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.comboBox1.KeyDown += new KeyEventHandler(combo1_TextChanged);
+            this.sock = null;
+
+            logFile = null;
+
+            unpickle();
+        }
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (sock != null)
+            {
+                sock.Client.Close();
+                sock.Close();
+                sock = null;
+                this.toolStripButton1.Text = "Connect";
+            }
+            else 
+            {
+                try
+                {
+                    int port = int.Parse(this.toolStripTextBox2.Text);
+                    // server:
+                    if (this.toolStripTextBox1.Text == null || this.toolStripTextBox1.Text == "" || this.toolStripTextBox1.Text == "0" || this.toolStripTextBox1.Text == "0.0.0.0")
+                    {
+                        this.textBox2.Text += "Waiting for client on " + port + "\r\n";
+                        TcpListener listener = new TcpListener(IPAddress.Any, port);
+                        listener.Start();
+                        sock = listener.AcceptTcpClient();
+                        write( "hello client " + nc++ );
+                        sock.Close();
+                        sock = null;
+                        listener.Stop();
+                        listener = null;
+                        this.textBox2.Text += "Connection on " + port  + " reset.\r\n";
+                        return;
+                    }
+                    else // client:
+                    {
+                        sock = new TcpClient(this.toolStripTextBox1.Text, port);
+                    }
+
+                    if (sock != null)
+                    {
+                        this.backgroundWorker1.RunWorkerAsync();
+                        this.toolStripButton1.Text = "Close";
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    this.textBox2.Text = ex.Message + "\r\n";
+                    if (sock != null)
+                    {
+                        this.backgroundWorker1.CancelAsync();
+                        sock.Client.Close();
+                        sock.Close();
+                        sock = null;
+                    }
+                }
+            }
+
+            if (!this.toolStripTextBox1.Items.Contains(this.toolStripTextBox1.Text))
+                this.toolStripTextBox1.Items.Add(this.toolStripTextBox1.Text);
+            if (!this.toolStripTextBox2.Items.Contains(this.toolStripTextBox2.Text))
+                this.toolStripTextBox2.Items.Add(this.toolStripTextBox2.Text);
+        }
+
+        private void read()
+        {
+            try
+            {
+                // Get the stream
+                Stream strm = sock.GetStream();
+                byte[] buf = new byte[1024];
+
+                // Read the stream and convert it to ASCII
+                if( strm.Read(buf, 0, 1024) > 0)
+                {
+                    string s = Encoding.ASCII.GetString(buf) + "\n";
+                    this.textBox2.Text += s;
+                    if (logFile != null)
+                        logFile.Write(s);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void write(string txt)
+        {
+            try
+            {
+                byte[] buf = Encoding.ASCII.GetBytes(txt.ToCharArray());
+                Stream strm = sock.GetStream();
+                strm.Write(buf, 0, buf.Length);
+            }
+            catch (Exception e) { }
+        }
+        
+        private void combo1_TextChanged(object sender, EventArgs e)
+        {
+            if ( (sock!= null) && (((KeyEventArgs)e).KeyValue == 13))
+            {
+                if ( this.comboBox1.Text != null)
+                {
+                    string txt = this.comboBox1.Text + "\r\n";
+                    write(txt);
+                    this.textBox2.Text += txt;
+
+                    if (!this.comboBox1.Items.Contains(this.comboBox1.Text))
+                        this.comboBox1.Items.Add(this.comboBox1.Text);
+                }
+
+                //read();
+            }
+        }
+
+        
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = "";
+        }
+
+
+        private void pickle()
+        {
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter("net.txt");
+                for (int i = 0; i < toolStripTextBox1.Items.Count; i++)
+                {
+                    file.WriteLine(toolStripTextBox1.Items[i].ToString());
+                }
+                file.WriteLine("#");
+                for (int i = 0; i < toolStripTextBox2.Items.Count; i++)
+                {
+                    file.WriteLine(toolStripTextBox2.Items[i].ToString());
+                }
+                file.WriteLine("#");
+                for (int i = 0; i < comboBox1.Items.Count; i++)
+                {
+                    file.WriteLine(comboBox1.Items[i].ToString());
+                }
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+        
+        
+        private void unpickle()
+        {
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader("net.txt");
+                while (true)
+                {
+                    string s = file.ReadLine();
+                    if (s == "#") break;
+                    toolStripTextBox1.Items.Add(s);
+                }
+                while (true)
+                {
+                    string s = file.ReadLine();
+                    if (s == "#") break;
+                    toolStripTextBox2.Items.Add(s);
+                }
+                while (true)
+                {
+                    string s = file.ReadLine();
+                    if (s == "#") break;
+                    comboBox1.Items.Add(s);
+                }
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (sock != null)
+            {
+                read();
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (logFile!=null)
+            {
+                logFile.Close();
+                logFile = null;
+            }
+            else
+            {
+                logFile = new System.IO.StreamWriter("net_log.txt", true);
+            }
+        }
+    }
+}
