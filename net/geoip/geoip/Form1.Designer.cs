172ab5a48c6a013384793481958a0ee104fea613
p4p4 <p4p4p4@web.de>
2011-02-17 15:12:25 +0100

    geoip

---
+++ net/geoip/geoip/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace geoip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            pickle();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ipCombo = new System.Windows.Forms.ToolStripComboBox();
+            this.findButton = new System.Windows.Forms.ToolStripButton();
+            this.zoomBox = new System.Windows.Forms.ToolStripComboBox();
+            this.labelL = new System.Windows.Forms.ToolStripButton();
+            this.labelU = new System.Windows.Forms.ToolStripButton();
+            this.labelD = new System.Windows.Forms.ToolStripButton();
+            this.labelR = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ipCombo,
+            this.findButton,
+            this.zoomBox,
+            this.labelL,
+            this.labelU,
+            this.labelD,
+            this.labelR});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(360, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ipCombo
+            // 
+            this.ipCombo.Name = "ipCombo";
+            this.ipCombo.Size = new System.Drawing.Size(181, 25);
+            this.ipCombo.Text = "88.4.5.6";
+            this.ipCombo.ToolTipText = "ip, host,  latlon or place name";
+            // 
+            // findButton
+            // 
+            this.findButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.findButton.Image = ((System.Drawing.Image)(resources.GetObject("findButton.Image")));
+            this.findButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(40, 22);
+            this.findButton.Text = "locate";
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // zoomBox
+            // 
+            this.zoomBox.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.zoomBox.AutoSize = false;
+            this.zoomBox.DropDownWidth = 45;
+            this.zoomBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17"});
+            this.zoomBox.Name = "zoomBox";
+            this.zoomBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.zoomBox.Size = new System.Drawing.Size(45, 21);
+            this.zoomBox.Text = "11";
+            this.zoomBox.ToolTipText = "zoom level";
+            // 
+            // labelL
+            // 
+            this.labelL.AutoSize = false;
+            this.labelL.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.labelL.Image = ((System.Drawing.Image)(resources.GetObject("labelL.Image")));
+            this.labelL.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.labelL.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.labelL.Name = "labelL";
+            this.labelL.Size = new System.Drawing.Size(18, 22);
+            this.labelL.Text = "<";
+            this.labelL.ToolTipText = "west";
+            this.labelL.Click += new System.EventHandler(this.labelL_Click);
+            // 
+            // labelU
+            // 
+            this.labelU.AutoSize = false;
+            this.labelU.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.labelU.Image = ((System.Drawing.Image)(resources.GetObject("labelU.Image")));
+            this.labelU.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.labelU.Name = "labelU";
+            this.labelU.Size = new System.Drawing.Size(18, 22);
+            this.labelU.Text = "^";
+            this.labelU.ToolTipText = "north";
+            this.labelU.Click += new System.EventHandler(this.labelU_Click);
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = false;
+            this.labelD.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.labelD.Image = ((System.Drawing.Image)(resources.GetObject("labelD.Image")));
+            this.labelD.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(18, 22);
+            this.labelD.Text = "v";
+            this.labelD.ToolTipText = "south";
+            this.labelD.Click += new System.EventHandler(this.labelD_Click);
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = false;
+            this.labelR.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.labelR.Image = ((System.Drawing.Image)(resources.GetObject("labelR.Image")));
+            this.labelR.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(18, 17);
+            this.labelR.Text = ">";
+            this.labelR.ToolTipText = "east";
+            this.labelR.Click += new System.EventHandler(this.labelR_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(360, 333);
+            this.splitContainer1.SplitterDistance = 78;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(0, 0);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(360, 78);
+            this.textBox.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 251);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 358);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "geolocator";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox ipCombo;
+        private System.Windows.Forms.ToolStripButton findButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripComboBox zoomBox;
+        private System.Windows.Forms.ToolStripButton labelR;
+        private System.Windows.Forms.ToolStripButton labelL;
+        private System.Windows.Forms.ToolStripButton labelU;
+        private System.Windows.Forms.ToolStripButton labelD;
+    }
+}
+
---
+++ net/geoip/geoip/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+
+
+namespace geoip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+            this.ipCombo.KeyDown += new KeyEventHandler( ipCombo_KeyDown );
+            unpickle();
+        }
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // break on escape, i can't live without it!
+            if (e.KeyChar == 27)
+                this.Close();
+        }
+
+
+        public void ipCombo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (((KeyEventArgs)e).KeyValue == 13)
+            {
+                if (this.ipCombo.Text != "")
+                {
+                    findButton.PerformClick(); // press it visibly
+                }
+            }
+        }
+
+        private void osmSearchCompleted(object sender, EventArgs e)
+        {
+            textBox.Enabled = true;
+            MapControl.SearchProvider search = (MapControl.SearchProvider)sender;
+            foreach (MapControl.SearchResult r in search.Results)
+            {
+                textBox.Text += r.DisplayName + "\r\n    " + r.Latitude + " " + r.Longitude + "\r\n";
+            }
+        }
+
+        private string findIp(string Hostname)
+        {
+            try
+            {
+                if (Uri.CheckHostName(Hostname) == UriHostNameType.Unknown)
+                {
+                    return null;
+                }
+                IPHostEntry host = Dns.GetHostEntry(Hostname);
+                return host.AddressList[0].ToString();
+            }
+            catch (Exception) { }
+            return null;
+        }
+        
+        // this is the free lookup, sometimes does not find stuff.
+        private string getlatlon(string ip)
+        {
+            string ll = null;
+            WebClient wc = new WebClient();
+            Stream str = wc.OpenRead("http://api.hostip.info/get_html.php?ip="+ip+"&position=true");
+            StreamReader sr = new StreamReader(str);
+            while ( true )
+            { 
+                string line = sr.ReadLine();
+                if ( line == null )
+                    break;
+                if ( line.StartsWith("Latitude: " ) )
+                    ll = line.Substring( 10 );
+                else
+                if ( line.StartsWith("Longitude: " ) )
+                    ll += " " + line.Substring( 11 );
+                else
+                    textBox.Text += line + "\r\n";
+            }
+            textBox.Text += "latlon: " + ll + "\r\n";
+            textBox.Text += "(fetched from http://api.hostip.info)\r\n";
+            return ll;
+        }
+
+        private string whois(string whoisServer, string url)
+        {
+            StringBuilder stringBuilderResult = new StringBuilder();
+            TcpClient tcpClinetWhois = new TcpClient(whoisServer, 43);
+            NetworkStream networkStreamWhois = tcpClinetWhois.GetStream();
+            BufferedStream bufferedStreamWhois = new BufferedStream(networkStreamWhois);
+            StreamWriter streamWriter = new StreamWriter(bufferedStreamWhois);
+
+            streamWriter.WriteLine(url);
+            streamWriter.Flush();
+
+            StreamReader streamReaderReceive = new StreamReader(bufferedStreamWhois);
+
+            while (!streamReaderReceive.EndOfStream)
+                stringBuilderResult.AppendLine(streamReaderReceive.ReadLine());
+
+            return stringBuilderResult.ToString();
+        }
+
+        private void getMap(string lat, string lon)
+        {
+            try
+            {   // find map for latlon:
+                string query = "http://tah.openstreetmap.org/MapOf/index.php?lat=" + lat + "&long=" + lon + "&z=" + zoomBox.Text + "&w=400&h=400&format=png";
+                query = query.Replace(',', '.'); // arghhh, i HATE localization!!!!!(german, in this case)
+                pictureBox1.Load(query);
+            }
+            catch (Exception ex) { textBox.Text += "\r\n\r\n" + ex.Message; return; }
+        }
+        
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if (textBox.SelectedText != "")
+            {   // check if a lat-lon was selected:
+                string[] ss = textBox.SelectedText.Split(" ".ToCharArray());
+                if (ss.Length == 2)
+                {
+                    if ((Double.Parse(ss[0]) != 0) && (Double.Parse(ss[1]) != 0))
+                    {
+                        ipCombo.Text = textBox.SelectedText;
+                    }
+                }
+            }
+            string lat = "", lon = "";
+            textBox.Text = "";
+            textBox.Enabled = false;
+
+            // chop copy&paste blanks:
+            while (ipCombo.Text.StartsWith(" "))
+               ipCombo.Text = ipCombo.Text.Remove(0, 1);
+
+            //check for ip or hostname
+            string ip = findIp(ipCombo.Text);
+            if (ip != null)
+            {
+                // check free lookup first
+                string ll = getlatlon(ip);
+                if (ll != null && ll != " ")
+                {
+                    string[] ss = ll.Split(" ".ToCharArray());
+                    if (ss.Length == 2)
+                    {
+                        if ((Double.Parse(ss[0]) != 0) && (Double.Parse(ss[1]) != 0))
+                        {
+                            lat = ss[0];
+                            lon = ss[1];
+                        }
+                        textBox.Enabled = true;
+                    }
+                }
+                if ( lat=="" && lon=="" )
+                {
+                    // restricted to 40 shots a day.
+                    IP2Geo geo = new IP2Geo();
+                    IPInformation res = null;
+                    try
+                    {   // find latlon from ip or hostname:
+                        res = geo.ResolveIP(ip, "0"); // 0 is the limited testkey.
+                        if (res == null) return;
+                        if (res.Longitude == 0 && res.Latitude == 0)
+                        {
+                            textBox.Text = res.Organization; // please wait or get a key ...
+                            return;
+                        }
+                        textBox.Enabled = true;
+                        textBox.Text = res.City + " (" + res.Country + ")\r\n" +
+                                       "latlon:" + res.Latitude + " " + res.Longitude + "\r\n" +
+                                       "ip:" + ip;
+                        lat = "" + res.Latitude;
+                        lon = "" + res.Longitude;
+                        textBox.Text += "\r\n(fetched from http://ws.cdyne.com)\r\n";
+                    }
+                    catch (Exception ex) { textBox.Text += "\r\n\r\n" + ex.Message; return; }
+                }
+                //textBox.Text += "\r\n--- whois " + ip + "\r\n";
+                //textBox.Text += whois("whois.internic.com", ip); ;
+            }
+
+            // check for latlon in input:
+            if (lat == "" && lon == "")
+            {
+                string[] ss = ipCombo.Text.Split(" ".ToCharArray());
+                if (ss.Length == 2)
+                {
+                    if ((Double.Parse(ss[0]) != 0) && (Double.Parse(ss[1]) != 0))
+                    {
+                        lat = ss[0];
+                        lon = ss[1];
+                    }
+                }
+            }
+            // still not found?! check osm for place names :
+            if (lat == "" && lon == "")
+            {
+                MapControl.SearchProvider search = new MapControl.SearchProvider();
+                search.SearchCompleted += new EventHandler(osmSearchCompleted);
+                if (search.Search(ipCombo.Text))
+                {
+                    textBox.Enabled = false;
+                }
+            }
+
+            if (lat != "" && lon != "")
+            {
+                getMap(lat, lon);
+            }
+            if (!this.ipCombo.Items.Contains(this.ipCombo.Text))
+                this.ipCombo.Items.Add(this.ipCombo.Text);
+        }
+
+        private void label_click(int id, double off)
+        {
+            string[] ss = ipCombo.Text.Split(" ".ToCharArray());
+            if (ss.Length == 2)
+            {
+                double [] latlon = new double[2] {Double.Parse(ss[0]),Double.Parse(ss[1])};
+                if ((latlon[0] != 0) && (latlon[1] != 0))
+                {
+                    double f = off / (18-Double.Parse(zoomBox.Text));
+                    ipCombo.Text = ipCombo.Text.Replace(ss[id], ("" + (latlon[id] + f)));
+                    getMap(ss[0], ss[1]);
+                }
+            }
+        }
+        private void labelL_Click(object sender, EventArgs e)
+        {
+            label_click(1, -0.02);
+        }
+        private void labelU_Click(object sender, EventArgs e)
+        {
+            label_click(0,  0.02);
+        }
+        private void labelD_Click(object sender, EventArgs e)
+        {
+            label_click(0, -0.02);
+        }
+        private void labelR_Click(object sender, EventArgs e)
+        {
+            label_click(1,  0.02);
+        }
+
+        private void pickle()
+        {
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter("ip.txt");
+                for (int i = 0; i < ipCombo.Items.Count; i++)
+                {
+                    file.WriteLine(ipCombo.Items[i].ToString());
+                }
+                file.WriteLine("#");
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+        private void unpickle()
+        {
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader("ip.txt");
+                while (true)
+                {
+                    string s = file.ReadLine();
+                    if (s == "#") break;
+                    ipCombo.Items.Add(s);
+                }
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+
+    }
+}
