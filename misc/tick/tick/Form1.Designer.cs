036e41551fa4152b48b8efee7e61743ec16b2e4f
p4p4 <p4p4p4@web.de>
2011-02-18 14:20:58 +0100

    some thing

---
+++ misc/tick/tick/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace tick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(98, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(161, 24);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Tick";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ misc/tick/tick/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace tick
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        static extern bool PlaySound( string pszSound,IntPtr hMod,SoundFlags sf);
+
+        [Flags]
+        public enum SoundFlags : int
+        {
+            SND_SYNC = 0x0000,  // play synchronously (default) 
+            SND_ASYNC = 0x0001,  // play asynchronously 
+            SND_NODEFAULT = 0x0002,  // silence (!default) if sound not found 
+            SND_MEMORY = 0x0004,  // pszSound points to a memory file
+            SND_LOOP = 0x0008,  // loop the sound until next sndPlaySound 
+            SND_NOSTOP = 0x0010,  // don't stop any currently playing sound 
+            SND_NOWAIT = 0x00002000, // don't wait if the driver is busy 
+            SND_ALIAS = 0x00010000, // name is a registry alias 
+            SND_ALIAS_ID = 0x00110000, // alias is a predefined ID
+            SND_FILENAME = 0x00020000, // name is file name 
+            SND_RESOURCE = 0x00040004  // name is resource name or atom 
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+        }
+
+        void Form1_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                button1_Click(null,null);
+            }
+            if (e.KeyChar == 27)
+            {
+                this.Close();
+            }
+        }
+
+        private void start()
+        {
+            timer1.Start();
+            this.button1.Text = "stop!";
+        }
+
+        private void stop()
+        {
+            timer1.Stop();
+            this.button1.Text = "go!";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int i = Int32.Parse(textBox1.Text);
+            if (i == 0)
+            {
+                System.Console.Beep(200, 600);
+                System.Console.Beep(400, 600);
+                System.Console.Beep(600, 600);
+                System.Console.Beep(800, 600);
+                PlaySound("mgnmpi.wav", IntPtr.Zero, SoundFlags.SND_ASYNC | SoundFlags.SND_FILENAME);
+                stop();
+            }
+            else
+            {
+                textBox1.Text = "" + (--i);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            if (this.button1.Text == "go!")
+            {
+                start();
+            }
+            else 
+            {
+                stop();
+            }
+        }
+    }
+}
